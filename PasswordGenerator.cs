f59e35b399db8f6a73223c99602214575545e4be
Mister Ky <140906950+Mister-Ky@users.noreply.github.com>
2023-10-23 18:11:48 +0300

    Update PasswordGenerator.cs

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -16,7 +16,6 @@ namespace Mister.Utils.Generator
             const string digitChars = "0123456789";
             const string specialChars = "!@#$%^&*()";
 
-            // Проверяем, достаточно ли символов для генерации пароля
             if (length < lowercaseCount + uppercaseCount + digitCount + specialCharCount)
             {
                 throw new ArgumentException("Недостаточная длина для указанных параметров.");
@@ -24,38 +23,32 @@ namespace Mister.Utils.Generator
 
             StringBuilder password = new StringBuilder(length);
 
-            // Генерируем символы в нижнем регистре
             for (int i = 0; i < lowercaseCount; i++)
             {
                 password.Append(lowercaseChars[random.Next(lowercaseChars.Length)]);
             }
 
-            // Генерируем символы в верхнем регистре
             for (int i = 0; i < uppercaseCount; i++)
             {
                 password.Append(uppercaseChars[random.Next(uppercaseChars.Length)]);
             }
 
-            // Генерируем цифры
             for (int i = 0; i < digitCount; i++)
             {
                 password.Append(digitChars[random.Next(digitChars.Length)]);
             }
 
-            // Генерируем специальные символы
             for (int i = 0; i < specialCharCount; i++)
             {
                 password.Append(specialChars[random.Next(specialChars.Length)]);
             }
 
-            // Заполняем оставшуюся часть пароля случайными символами
             for (int i = password.Length; i < length; i++)
             {
                 string availableChars = lowercaseChars + uppercaseChars + digitChars + specialChars;
                 password.Append(availableChars[random.Next(availableChars.Length)]);
             }
 
-            // Перемешиваем символы пароля
             string generatedPassword = new string(password.ToString().OrderBy(x => random.Next()).ToArray());
 
             return generatedPassword;
